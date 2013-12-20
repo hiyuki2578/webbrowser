1aa986ad2ab5ef51cccc1aa150c46d57018456c7
sakura2578 <tomoka319@gmail.com>
2013-12-20 11:47:39 +0900

    Initial commit

---
+++ webbrowser/Main.cs
@@ -0,0 +1,17 @@
+using System;
+using Gtk;
+using WebKit;
+
+namespace webkit
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
---
+++ webbrowser/MainWindow.cs
@@ -0,0 +1,53 @@
+using System;
+using Gtk;
+using WebKit;
+
+public partial class MainWindow: Gtk.Window
+{	
+	WebView webview = new WebView ();
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		scrolledwindow1.Add (webview);
+		webview.Show();
+		webview.Open("http://google.com");
+	}
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+	protected void reload (object sender, EventArgs e)
+	{
+		webview.Reload ();
+	}
+	protected void back (object sender, EventArgs e)
+	{
+		webview.GoBack ();
+	}
+	protected void go (object sender, EventArgs e)
+	{
+		webview.GoForward ();
+	}
+	protected void home (object sender, EventArgs e)
+	{
+		webview.Open("http://google.com");
+	}
+	protected void stop (object sender, EventArgs e)
+	{
+		webview.StopLoading();
+	}
+	private void webview_Navigated (object sender, WebNavigationAction e)
+	{
+		try {
+			entry1.Text = webview.Uri.ToString();
+		}
+		catch 
+		{
+		}
+	}
+	protected void url_go (object sender, EventArgs e)
+	{
+		webview.Open (entry1.Text);
+	}
+}
